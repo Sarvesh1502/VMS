301b2894e5549cb2bfd23348a8540609ebc59df2
Sarvesh1502 <Sarveshv2006@gmail.com>
2024-11-28 20:08:33 +0530

    commit

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; // The main App component
-import "./index.css"; // Optional: Global CSS (if you have it)
+//import "./index.css"; // Optional: Global CSS (if you have it)
 import { BrowserRouter as Router } from "react-router-dom"; // Import Router for routing in the app
 
 // Render the App component inside the root element in your index.html
